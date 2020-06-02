99c21e88135c810ef7ae62176bf18f2b0f8867fa
20176690.NguyenQuangAnh <quanganhk62@gmail.com>
2020-06-02 21:26:08 +0700

    SortArray

--- MySort/src/sortVisualier/SortVisualiser.java
+++ MySort/src/sortVisualier/SortVisualiser.java
@@ -3,20 +3,23 @@ package sortVisualier;
 import javax.swing.JFrame;
 
 public class SortVisualiser {
-	public static final int WIN_WIDTH = 1200;
-	public static final int WIN_HEIGHT = 720;
-	
-	private JFrame windowFrame;
-	
+
+	private JFrame window;
+	private SortArray sortArray;
+
 	public SortVisualiser() {
-		windowFrame = new JFrame("Sort Visualiser");
-		windowFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		windowFrame.setSize(WIN_WIDTH,WIN_HEIGHT);
-		windowFrame.setVisible(true);
+		window = new JFrame("Sort Visualiser");
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setSize(SortArray.WIN_WIDTH, SortArray.WIN_HEIGHT);
+		window.setVisible(true);
+
+		sortArray = new SortArray();
+		window.add(sortArray);
+		sortArray.repaint();
 	}
-	
-	public static void main(String []args) {
+
+	public static void main(String[] args) {
 		SortVisualiser sortVisualiser = new SortVisualiser();
-		sortVisualiser.toString();
+		//sortVisualiser.toString();
 	}
 }
